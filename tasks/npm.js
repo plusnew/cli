d8fb63bbbc7207ec009a7be0cb28487a75d3280c
plusgut <github@webentwickler2-0.de>
2017-12-27 19:13:26 +0100

    [packages] remove static dependencies and added correct config path for webpack build

--- tasks/npm.js
+++ tasks/npm.js
@@ -2,15 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const { spawnSync } = require('child_process');
 
+const DEPENDENCIES = ['plusnew'];
+const DEV_DEPENDENCIES = ['webpack','typescript', 'tslint', 'tslint-config-airbnb']
+
 function changeProjectname(dest, projectName) {
   const packagePath = path.join(dest, 'package.json');
   const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-  package.name = projectName;
-  fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
+  if (package.name === '') {
+    package.name = projectName;
+    fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
+  }
 }
 
 function installDependencies(dest) {
-  spawnSync('npm', ['install'], { cwd: dest, stdio: 'inherit' })
+  DEPENDENCIES.forEach(installDependency.bind(null, false, dest));
+  DEV_DEPENDENCIES.forEach(installDependency.bind(null, true, dest));
+}
+
+function installDependency(dev, dest, package) {
+  spawnSync('npm', ['install', package, dev ? '--save-dev' : '--save'], { cwd: dest, stdio: 'inherit' })
 }
 
 module.exports = function(dest, projectName) {
