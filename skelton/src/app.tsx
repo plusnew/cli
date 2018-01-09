d29de04c8c69353a83612cfb44ab6f9d9d1312bc
plusgut <github@webentwickler2-0.de>
2018-01-09 18:33:32 +0100

    [store] changed to new store api

--- skelton/src/app.tsx
+++ skelton/src/app.tsx
@@ -1,10 +1,17 @@
-import plusnew, { component } from 'plusnew';
+import plusnew, { component, store } from 'plusnew';
 
-const component: component<{}> = () => {
+const component: component = () => {
+  const local = store(0, (state) => state + 1)();
+
   return {
-    dependencies: {},
-    render: () => <div>New plusnew application</div>,
-  }
+    dependencies: { local },
+    render: () =>
+      <div>
+        <button onclick={local.dispatch}>count</button>
+        {local.state}
+      </div>,
+  };
 };
 
 export default component;
+
