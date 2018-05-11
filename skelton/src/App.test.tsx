171d572ab73f6177d0a06013dd9a342464f7f4f3
plusgut <github@webentwickler2-0.de>
2018-05-11 16:15:05 +0200

    [api] changed to new plusnew api

--- skelton/src/App.test.tsx
+++ skelton/src/App.test.tsx
@@ -1,5 +1,14 @@
+import enzymeAdapterPlusnew, { mount } from 'enzyme-adapter-plusnew';
+import { configure } from 'enzyme';
+import plusnew from 'plusnew';
+import App from './App';
+
+configure({ adapter: new enzymeAdapterPlusnew() });
+
 describe('test App.tsx', () => {
   it('true should be true', () => {
-    expect(true).toBe(true);
+    const wrapper = mount(<App />);
+    expect(wrapper.find(<button />).length).toBe(1);
   });
 });
+
--- skelton/src/App.tsx
+++ skelton/src/App.tsx
@@ -1,6 +1,7 @@
 import plusnew, { component, store } from 'plusnew';
 
 export default component(
+  'App',
   () => ({
     local: store(0, (state, action: number) => state + action),
   }),
