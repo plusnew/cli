30dd511ca3aa17ffbf4811ad9648b3ac1459378b
plusgut <github@webentwickler2-0.de>
2019-07-21 18:54:35 +0200

    [version] bump

--- skelton/src/App.tsx
+++ skelton/src/App.tsx
@@ -3,10 +3,19 @@ import plusnew, { component, store } from 'plusnew';
 export default component(
   'App',
   () => {
-    const local = store(0, (state, action: number) => state + action);
+    const local = store(0, (state, action: 'increment' | 'decrement') => {
+      if (action === 'increment') {
+        return state + 1;
+      }
+      if (action === 'decrement') {
+        return state - 1;
+      }
+      throw new Error('No such action found');
+    });
     return (
       <div>
-        <button onclick={() => local.dispatch(2)}>count</button>
+        <button onclick={() => local.dispatch('increment')}>up</button>
+        <button onclick={() => local.dispatch('down')}>up</button>
         <local.Observer>{state => state}</local.Observer>
       </div>
     );
