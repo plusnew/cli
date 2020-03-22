070adcbec398c7d2d433609db8c7426866aee798
plusgut <github@webentwickler2-0.de>
2020-03-22 13:46:55 +0100

    [npm] 1.0.0

--- tasks/npm.js
+++ tasks/npm.js
@@ -2,49 +2,49 @@ const fs = require('fs');
 const path = require('path');
 const { spawnSync } = require('child_process');
 
-const DEPENDENCIES = ['@plusnew/core@beta', '@plusnew/driver-dom'];
+const DEPENDENCIES = [ '@plusnew/core', '@plusnew/driver-dom' ];
 const DEV_DEPENDENCIES = [
-  '@plusnew/simulate-dom-events',
-  '@plusnew/enzyme-adapter@beta',
-  '@types/jasmine',
-  'awesome-typescript-loader',
-  'clean-webpack-plugin',
-  'css-loader',
-  'enzyme',
-  'file-loader',
-  'html-webpack-plugin',
-  'identity-obj-proxy',
-  'jest',
-  'mini-css-extract-plugin',
-  'node-sass',
-  'sass-loader',
-  'script-ext-html-webpack-plugin',
-  'ts-jest',
-  'typescript',
-  'webpack',
-  'webpack-cli',
-  'webpack-dev-server',
+	'@plusnew/simulate-dom-events',
+	'@plusnew/enzyme-adapter',
+	'@types/jasmine',
+	'awesome-typescript-loader',
+	'clean-webpack-plugin',
+	'css-loader',
+	'enzyme',
+	'file-loader',
+	'html-webpack-plugin',
+	'identity-obj-proxy',
+	'jest',
+	'mini-css-extract-plugin',
+	'node-sass',
+	'sass-loader',
+	'script-ext-html-webpack-plugin',
+	'ts-jest',
+	'typescript',
+	'webpack',
+	'webpack-cli',
+	'webpack-dev-server'
 ];
 
 function changeProjectname(dest, projectName) {
-  const packagePath = path.join(dest, 'package.json');
-  const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-  if (package.name === '') {
-    package.name = projectName;
-    fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
-  }
+	const packagePath = path.join(dest, 'package.json');
+	const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+	if (package.name === '') {
+		package.name = projectName;
+		fs.writeFileSync(packagePath, JSON.stringify(package, null, 2));
+	}
 }
 
 function installDependencies(dest) {
-  installDependency(dest, false, DEPENDENCIES);
-  installDependency(dest, true, DEV_DEPENDENCIES);
+	installDependency(dest, false, DEPENDENCIES);
+	installDependency(dest, true, DEV_DEPENDENCIES);
 }
 
 function installDependency(dest, dev, packages) {
-  spawnSync('npm', ['install', ...packages, dev ? '--save-dev' : '--save'], { cwd: dest, stdio: 'inherit' });
+	spawnSync('npm', [ 'install', ...packages, dev ? '--save-dev' : '--save' ], { cwd: dest, stdio: 'inherit' });
 }
 
 module.exports = function(dest, projectName) {
-  changeProjectname(dest, projectName);
-  installDependencies(dest);
-}
+	changeProjectname(dest, projectName);
+	installDependencies(dest);
+};
