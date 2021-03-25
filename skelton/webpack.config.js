9340b42627d9e12123ed3bd4977ec6c01d43f24e
plusgut <github@webentwickler2-0.de>
2021-03-25 18:37:43 +0100

    [yarn] update

--- skelton/webpack.config.js
+++ skelton/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -8,12 +7,12 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 module.exports = (env, argv) => ({
 	context: path.join(__dirname, 'src'),
 	entry: [ './index.tsx' ],
-	mode: argv.mode,
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'js/[name].[hash].js',
 		chunkFilename: 'js/[name].[hash].bundle.js',
-		publicPath: '/'
+		publicPath: '/',
+		clean: true
 	},
 	resolve: {
 		extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
@@ -72,9 +71,6 @@ module.exports = (env, argv) => ({
 		}
 	},
 	plugins: [
-		new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns: [ path.join(__dirname, 'dist') ]
-		}),
 		new HtmlWebpackPlugin({
 			title: 'plusnew app',
 			inject: 'head'
